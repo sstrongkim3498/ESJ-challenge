1947e7d53dd489189b7bd03a618a35ec57cdc60a
kimsejin <kimsj3498@gmail.com>
2021-12-27 08:53:57 +0900

    1~8 course

--- app.js
+++ app.js
@@ -10,23 +10,32 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  res.render("home", {startingContent: homeStartingContent})
+});
 
+app.get("/about", function(req, res) {
+  res.render("about", {aboutContent: aboutContent})
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactContent: contactContent})
+});
 
+app.get("/compose", function(req, res) {
+  res.render("compose")
+});
 
-
-
-
-
-
-
-
-
+app.post("/compose", function(req, res) {
+  console.log(req.body.postTitle);
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
